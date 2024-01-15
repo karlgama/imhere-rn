59a43877c92da14460d6c62a8d8cd1e96e5e3a14
kaique <kaiquesilva49@hotmail.com>
2024-01-15 20:57:07 -0300

    form

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+export const styles =StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#131016'
+    },
+    input:{
+        flex:1,
+        height:56,
+        backgroundColor:'#1F1E25',
+        borderRadius:5,
+        color:'#FFF',
+        padding:16,
+        marginRight:12
+    },
+    button:{
+        borderRadius:5,
+        width:56,
+        height:56,
+        backgroundColor:'#31cf67',
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    buttonText:{
+        color:'#fff',
+        fontSize:24
+    },
+    eventDate:{
+
+    },
+    eventName:{},
+    form:{
+        width:'100%',
+        flexDirection:'row',
+        marginTop:36,
+        marginBottom:42
+    }
+})
